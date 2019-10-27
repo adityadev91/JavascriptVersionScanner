6e0cd4138a360e9af98a8ee7c006bffc059dd24c
Aditya Dev <AdityaDevG@nulogy.com>
2019-10-26 23:10:11 -0400

    Composed the script to their respective different functions

--- scanner.rb
+++ scanner.rb
@@ -5,30 +5,50 @@ require_relative "environment_variables"
 module JSLibraryScanner
   class ScannerUtils
     def self.run
-      parse_list_of_js_file_paths
+      hash_library_version = get_versions_of_js_libraries
+      pp hash_library_version
     end
 
     def self.get_list_of_js_file_paths
       Dir.glob($JS_PATH)
     end
 
-    def self.parse_list_of_js_file_paths
+    def self.get_versions_of_js_libraries
       list_of_js_file_paths = get_list_of_js_file_paths
-      list_of_js_file_paths.map { |js_file_path|
-        puts js_file_path.split("/")[-1]
-        js_file_content = File.open(js_file_path).read
-        js_file_content.each_line do |line|
-          if line.downcase.include? "version" # and (line.include? ":" or line.include? "=")
-            version = line.scan(/\d+\.[a-z0-9]+\.[a-z0-9|-]+/)
-            if version.length > 0
-              puts version
-            else
-              puts "This JS file probably contains the version number that cannot be parsed with your current regex. Please update your RegExp pattern"
-            end
+      parse_js_file_content(list_of_js_file_paths)
+    end
+
+    def self.get_file_name(file_path)
+      file_path.split("/")[-1]
+    end
+
+    def self.parse_js_file_content(file_paths)
+      hash_library_version = Hash.new("")
+      file_paths.map do |file_path|
+        js_file_content = get_file_content(file_path)
+        file_name = get_file_name(file_path)
+        hash_library_version[file_name] = get_version_from_file_content(js_file_content)
+      end
+      hash_library_version
+    end
+
+    def self.get_version_from_file_content(file_content)
+      list_of_versions = []
+      file_content.each_line do |line|
+        if line.downcase.include? "version"
+          version = line.scan(/\d+\.[a-z0-9]+\.[a-z0-9|-]+/)
+          if version.length > 0
+            list_of_versions.push version
+          else
+            list_of_versions.push "Probably better regexp needed?"
           end
         end
-        puts ""
-      }
+      end
+      list_of_versions
+    end
+
+    def self.get_file_content(file_path)
+      File.open(file_path).read
     end
   end
 end
